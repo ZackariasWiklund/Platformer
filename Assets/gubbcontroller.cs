aecfbab41fdfe84a02f20374af70ce045218955f
Zackarias Wikund <zackarias.wiklund@elev.ga.ntig.se>
2023-10-11 15:55:41 +0200

    initial commit

---
+++ Assets/gubbcontroller.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gubbcontroller : MonoBehaviour
+{
+
+    [SerializeField]
+    float Speed = 10;
+
+    [SerializeField]
+    float jumpForce = 100;
+
+    [SerializeField]
+    Transform feet;
+
+    [SerializeField]
+    LayerMask groundLayer;
+    Rigidbody2D rigidbody;
+
+    [SerializeField]
+    float groundRadius = 0.2f;
+
+
+    bool hasReleasedJumpButton = true;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        float moveX = Input.GetAxisRaw("Horizontal");
+
+        Vector2 movementX = new Vector2(moveX, 0);
+
+        transform.Translate(movementX * Speed * Time.deltaTime);
+
+        bool isGrounded = Physics2D.OverlapCircle(feet.position, groundRadius, groundLayer);
+
+        Debug.Log(isGrounded);
+
+
+        if (Input.GetAxisRaw("Jump") > 0 && hasReleasedJumpButton == true && isGrounded)
+        {
+            
+            rigidbody.AddForce(Vector2.up * jumpForce);
+            hasReleasedJumpButton = false;
+        }
+
+        if (Input.GetAxisRaw("Jump") == 0)
+        {
+            hasReleasedJumpButton = true;
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (feet)
+        {
+            Gizmos.DrawWireSphere(feet.position, groundRadius);
+        }
+    } 
+
+}
